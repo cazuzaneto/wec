58e6169a29fb93fecf0ab8dd5e0d88a6cd41d123
l3co <lcosta.sp.br@gmail.com>
2024-04-07 15:23:01 -0300

    refactor(driver) : change carID to car response and car request

--- src/main/kotlin/tech/jaya/wec/dto/driver/DriverRequest.kt
+++ src/main/kotlin/tech/jaya/wec/dto/driver/DriverRequest.kt
@@ -1,25 +1,20 @@
 package tech.jaya.wec.dto.driver
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import tech.jaya.wec.dto.car.CarRequest
 import tech.jaya.wec.model.Driver
 
 data class DriverRequest(
     @JsonProperty("name") val name: String,
     @JsonProperty("available") val available: Boolean,
-    @JsonProperty("carId") val carId: Long?
+    @JsonProperty("car") val car: CarRequest
 ) {
-
-    constructor(driver: Driver) : this(
-        name = driver.name,
-        available = driver.available,
-        carId = driver.car?.id
-    )
 
     fun toEntity(): Driver {
         return Driver(
             name = this.name,
             available = this.available,
-            car = null
+            car = this.car.toEntity()
         )
     }
 }
--- src/main/kotlin/tech/jaya/wec/dto/driver/DriverResponse.kt
+++ src/main/kotlin/tech/jaya/wec/dto/driver/DriverResponse.kt
@@ -1,19 +1,21 @@
 package tech.jaya.wec.dto.driver
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import tech.jaya.wec.dto.car.CarResponse
+import tech.jaya.wec.dto.car.toResponse
 import tech.jaya.wec.model.Driver
 
 data class DriverResponse(
     @JsonProperty("id") val id: Long,
     @JsonProperty("name") val name: String,
     @JsonProperty("available") val available: Boolean,
-    @JsonProperty("carId") val carId: Long?
+    @JsonProperty("car") val car: CarResponse?
 ) {
     constructor(driver: Driver) : this(
         id = driver.id ?: throw IllegalArgumentException("Driver id cannot be null"),
         name = driver.name,
         available = driver.available,
-        carId = driver.car?.id
+        car = driver.car?.toResponse()
     )
 }
 
