0ad997447f2d70e91ce38510b2e2f9702ec53154
leco <l3co@outlook.com>
2024-04-10 18:13:00 -0300

    refactor(driver): inject dao instead service

--- src/main/kotlin/tech/jaya/wec/controller/DriverController.kt
+++ src/main/kotlin/tech/jaya/wec/controller/DriverController.kt
@@ -1,44 +1,30 @@
 package tech.jaya.wec.controller
 
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import tech.jaya.wec.dao.DriverDao
 import tech.jaya.wec.model.Driver
-import tech.jaya.wec.service.DriverService
 
 @RestController
 @RequestMapping("/drivers")
-class DriverController(private val driverService: DriverService) {
-
-    @GetMapping
-    fun findAll(): ResponseEntity<List<Driver>> {
-        return driverService.findAll().let {
-            ResponseEntity.ok(it)
-        }
-    }
+class DriverController(private val driverDao: DriverDao) {
 
     @GetMapping("/{id}")
     fun findById(@PathVariable id: Long): ResponseEntity<Driver> {
-        return driverService.findById(id)?.let {
+        return driverDao.findById(id)?.let {
             ResponseEntity.ok(it)
         } ?: ResponseEntity.notFound().build()
     }
 
     @PostMapping
     fun save(@RequestBody driverRequest: Driver): ResponseEntity<Driver> {
-        return driverService.save(driverRequest).let {
+        return driverDao.save(driverRequest).let {
             ResponseEntity.ok(it)
         }
-    }
-
-    @DeleteMapping("/{id}")
-    fun deleteById(@PathVariable id: Long): ResponseEntity<Unit> {
-        driverService.deleteById(id)
-        return ResponseEntity.noContent().build()
     }
 }
